edb0537e7d7ec831aede45f0fbe3429bd8e566f6
LauraB <realityisasimulation@gmail.com>
2022-08-18 16:18:16 -0700

    Removed SectionList work until we update the data

--- src/screens/BrowseCCScreen.js
+++ src/screens/BrowseCCScreen.js
@@ -25,13 +25,6 @@ const BrowseCCScreen = ({route}) => {
       />
     );
   };
-
-//   const Item = ({ part }) => (
-//     <View style={sty.item}>
-//       <Text style={sty.title}>{part}</Text>
-//     </View>
-//   );
-  
   return (
     <View style={[styles.background, styles.container]}>
       <FlatList
@@ -42,38 +35,11 @@ const BrowseCCScreen = ({route}) => {
       />
 
 
-
     </View>
-
-
-
 
   );
 };
 
-const sty = StyleSheet.create({
-    screen: {
-      marginTop: 18,
-    },
-    header: {
-      fontSize: 30,
-      color: "#FFF",
-      marginTop: 10,
-      padding: 2,
-      backgroundColor: "#C2185B",
-      textAlign: "center",
-    },
-    row: {
-        marginHorizontal: 15,
-        marginTop: 10,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        paddingHorizontal: 2,
-      },
-      rowText: {
-        fontSize: 18,
-      },
-  });
+
 
 export default BrowseCCScreen;
