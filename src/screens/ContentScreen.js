baeb1afee6bb7ba8a1fff875451a15d0bbbd01a2
ehubwork <ehubwork1@gmail.com>
2022-08-18 11:17:54 -0700

    MVA content screen update

--- src/screens/ContentScreen.js
+++ src/screens/ContentScreen.js
@@ -3,47 +3,86 @@
 */
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Text, View, Pressable} from 'react-native';
+import {Text, View, Pressable, StyleSheet} from 'react-native';
 
 import Bookmark from '../components/Bookmark';
 import styles from '../assets/styles';
 
 function ContentScreen({route}) {
-  const { item } = route.params;
+  const {item} = route.params;
 
-  return(
-    
-    <View>
-      <Text>{item.contravention}</Text>
-      <Text>{item.source}</Text>
-      <Text>Section {item.provision}</Text>
-      <Text>{item.sectionText}</Text>
-      <Text>{item.sectionSubsection}</Text>
-      <Text>{item.sectionParagraph}</Text>
-      <Text>{item.sectionSubparagraph}</Text>
-      <Text>Ticketed Amount: {item.fine}</Text>
-      <Text>Reduced ticked amount: {item.reducedFine}</Text>
+
+  return (
+    <View style={styles.background}>
+      <View style={styles.sectionDivider} />
+      <Text style={[styles.heading_1, styles.neutral]}>
+        DEV screen name: MVA CONTENT
+      </Text>
+      <View style={stylesContent.headingContainer}>
+        <View style={stylesContent.headingContainerLeft}>
+          <Text style={{...styles.heading_1, fontWeight: 'bold'}}>
+            {item.contravention}
+          </Text>
+        </View>
+        <View style={stylesContent.headingContainerRight}>
+          <Bookmark />
+        </View>
+      </View>
+      <View style={stylesContent.contentSection}>
+        <Text style={styles.accent_1}>
+          {item.source}, Section {item.provision}
+        </Text>
+        <Text style={stylesContent.contentSectionText}>{item.sectionText}</Text>
+        <Text></Text>
+        <View style={stylesContent.contentSubsection}>
+          <Text>{item.sectionSubsection}</Text>
+          <Text>{item.sectionParagraph}</Text>
+          <Text>{item.sectionSubparagraph}</Text>
+          <Text>{item.sectionSubLength}</Text>
+        </View>
+      </View>
+
+      <View>
+        <Text style={stylesContent.contentTicket}>
+          Ticketed Amount: {item.fine}
+        </Text>
+        <Text style={stylesContent.contentTicket}>
+          Reduced ticked amount ({'<'}30 days): {item.reducedFine}
+        </Text>
+      </View>
     </View>
-  )
+  );
 }
 
-// const ContentScreen = props => {
-//   return (
-//     <SafeAreaView>
-//       <View style={styles.background}>
-//         <View style={styles.sectionDivider} />
-//         <Text style={[styles.heading_1, styles.neutral]}>
-//           DEV screen name: MVA CONTENT
-//         </Text>
-//         <Text style={styles.heading_2}>Heading 2</Text>
-//         <Bookmark />
-//         <Text style={[styles.body, styles.accent_1]}>Body</Text>
-//         <Text style={[styles.body, styles.accent_2]}>Body 2</Text>
-//         <Text>{navigation.getParam('source')}</Text>
-//       </View>
-//     </SafeAreaView>
-//   );
-// };
+const stylesContent = StyleSheet.create({
+  headingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 18,
+    paddingTop: 18,
+  },
+  headingContainerLeft: {
+    flex: 1,
+    width: '95%',
+  },
+  headingContainerRight: {
+    alignItems: 'flex-end',
+  },
+  contentSection: {
+    paddingHorizontal: 18,
+  },
+  contentSectionText: {
+    paddingTop: 18
+  },
+  contentSubsection: {
+    paddingHorizontal: 18,
+
+  },
+  contentTicket: {
+    paddingHorizontal: 18,
+    fontWeight: 'bold',
+    fontSize: 15
+  },
+});
 
 export default ContentScreen;
-
