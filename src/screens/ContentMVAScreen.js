e93afa09ea04c9e74c766067cdeb02a13fc5e77d
doolaik <doolaik@users.noreply.github.com>
2022-08-22 15:41:41 -0700

    Revert "Merge pull request #7 from OrganizedIsolatedInsect/Update-ContentMVAScreen-and-Components"
    This reverts commit 9fb8d821c5103c055ca328f2ee7a207b167f2692, reversing
    changes made to 727221d0e03e0515de714456b63bfa0be18dd9c0.

--- src/screens/ContentMVAScreen.js
+++ src/screens/ContentMVAScreen.js
@@ -1,11 +1,29 @@
+/*
+
+*/
 import React from 'react';
-import ContentMVA from '../components/ContentMVA';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Text, View, Pressable} from 'react-native';
 
-const ContentMVAScreen = ({route}) => {
-  const item = route.params.paramkey;
+import Bookmark from '../components/Bookmark';
+import styles from '../assets/styles';
 
-
-  return <ContentMVA item={item} />;
-}
+const ContentMVAScreen = props => {
+  return (
+    <SafeAreaView>
+      <View style={styles.background}>
+        <View style={styles.sectionDivider} />
+        <Text style={[styles.heading_1, styles.neutral]}>
+          DEV screen name: MVA CONTENT
+        </Text>
+        <Text style={styles.heading_2}>Heading 2</Text>
+        <Bookmark />
+        <Text style={[styles.body, styles.accent_1]}>Body</Text>
+        <Text style={[styles.body, styles.accent_2]}>Body 2</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default ContentMVAScreen;
+
