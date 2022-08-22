0953637820fac18a85d114a3495d020e217102e4
LauraB <realityisasimulation@gmail.com>
2022-08-22 14:08:32 -0700

    gos

--- src/screens/BrowseCCScreen.js
+++ src/screens/BrowseCCScreen.js
@@ -8,10 +8,67 @@ import { View, FlatList, SectionList, StyleSheet } from 'react-native';
 import {CCDATAPARTS} from '../data/dummy-data'; //for DEVELOPMENT Purposes
 import styles from '../assets/styles';
 import CrimCodeGridList from '../components/CrimCodeGridList';
+import groupBy from 'json-groupby';
 
-const BrowseCCScreen = ({route}) => {
+
+const BrowseCCScreen = (props) => {
   const [isLoading, setIsLoading] = useState(false); //for loading spinner
 
+
+const result = (CCDATAPARTS);
+
+
+const TestSection = () =>{
+
+//  console.log(groupBy(result, ['part'],['section'],['index'],['sectionHeader']));
+
+ 
+
+ const newArray = [];
+ newArray.push(result);
+
+ newArray.forEach
+
+// result.forEach(function(part, i){
+//   newArray.push(result[part, i])
+
+// });
+
+
+
+// function groupBy(objectArray, property) {
+//   return objectArray.reduce((acc, obj) => {
+//     const key = obj[property];
+//     acc[key] ??= [];
+//     acc[key].push(obj);
+//     return acc;
+//   }, {});
+// };
+
+//const reformattedArray = groupBy(newArray, 'part' )
+
+
+// let newDirectory = Object.values(newArray.reduce((acc, item) => {
+//   if (!acc[item.part]) acc[item.part] = {
+//     part: item.part,
+//       data: [item.index, item.section, item.sectionHeader]
+//   };
+//   acc[item.part].data.push(item.data);
+//   return acc;
+// }, {}));
+
+
+ console.log(result);
+  // const newArray = result.map(function(item) {
+  //   return {
+  //       key: item.part,
+  //       label: item.section
+  //   }
+
+  // });
+  // console.log(newArray);
+
+};
 
 
   const renderList = itemdata => {
