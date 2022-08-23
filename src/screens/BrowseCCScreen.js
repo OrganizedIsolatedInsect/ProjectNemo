d10c2222b584f053b1f0188fa67ffed8e85bcde5
OrganizedIsolatedInsect <organizedisolatedinsect@gmail.com>
2022-08-22 17:09:36 -0700

    initial commit

--- src/screens/BrowseCCScreen.js
+++ src/screens/BrowseCCScreen.js
@@ -2,77 +2,63 @@
  */
 
 import React, {useState} from 'react';
-import { View, FlatList, SectionList, StyleSheet } from 'react-native';
+import {View, FlatList, SectionList, StyleSheet} from 'react-native';
 
 //USER Imports
 import {CCDATAPARTS} from '../data/dummy-data'; //for DEVELOPMENT Purposes
 import styles from '../assets/styles';
 import CrimCodeGridList from '../components/CrimCodeGridList';
-import groupBy from 'json-groupby';
 
-
-const BrowseCCScreen = (props) => {
+const BrowseCCScreen = props => {
   const [isLoading, setIsLoading] = useState(false); //for loading spinner
 
+  const result = CCDATAPARTS;
 
-const result = (CCDATAPARTS);
+  const TestSection = () => {
+    //  console.log(groupBy(result, ['part'],['section'],['index'],['sectionHeader']));
 
+    const newArray = [];
+    newArray.push(result);
 
-const TestSection = () =>{
+    newArray.forEach;
 
-//  console.log(groupBy(result, ['part'],['section'],['index'],['sectionHeader']));
+    // result.forEach(function(part, i){
+    //   newArray.push(result[part, i])
 
- 
+    // });
 
- const newArray = [];
- newArray.push(result);
+    // function groupBy(objectArray, property) {
+    //   return objectArray.reduce((acc, obj) => {
+    //     const key = obj[property];
+    //     acc[key] ??= [];
+    //     acc[key].push(obj);
+    //     return acc;
+    //   }, {});
+    // };
 
- newArray.forEach
+    //const reformattedArray = groupBy(newArray, 'part' )
 
-// result.forEach(function(part, i){
-//   newArray.push(result[part, i])
+    // let newDirectory = Object.values(newArray.reduce((acc, item) => {
+    //   if (!acc[item.part]) acc[item.part] = {
+    //     part: item.part,
+    //       data: [item.index, item.section, item.sectionHeader]
+    //   };
+    //   acc[item.part].data.push(item.data);
+    //   return acc;
+    // }, {}));
 
-// });
+    console.log(result);
+    // const newArray = result.map(function(item) {
+    //   return {
+    //       key: item.part,
+    //       label: item.section
+    //   }
 
-
-
-// function groupBy(objectArray, property) {
-//   return objectArray.reduce((acc, obj) => {
-//     const key = obj[property];
-//     acc[key] ??= [];
-//     acc[key].push(obj);
-//     return acc;
-//   }, {});
-// };
-
-//const reformattedArray = groupBy(newArray, 'part' )
-
-
-// let newDirectory = Object.values(newArray.reduce((acc, item) => {
-//   if (!acc[item.part]) acc[item.part] = {
-//     part: item.part,
-//       data: [item.index, item.section, item.sectionHeader]
-//   };
-//   acc[item.part].data.push(item.data);
-//   return acc;
-// }, {}));
-
-
- console.log(result);
-  // const newArray = result.map(function(item) {
-  //   return {
-  //       key: item.part,
-  //       label: item.section
-  //   }
-
-  // });
-  // console.log(newArray);
-
-};
-
+    // });
+    // console.log(newArray);
+  };
 
   const renderList = itemdata => {
-
     return (
       <CrimCodeGridList
         index={itemdata.item.index}
@@ -90,13 +76,8 @@ const TestSection = () =>{
         keyExtractor={item => item.index}
         ItemSeparatorComponent={() => <View style={{margin: 15}} />}
       />
-
-
     </View>
-
   );
 };
-
-
 
 export default BrowseCCScreen;
