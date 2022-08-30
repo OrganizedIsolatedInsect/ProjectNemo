187cb2f51d217f3b701dd53092457857ebb483cf
doolaik <doolaik@users.noreply.github.com>
2022-08-30 16:16:26 -0700

    Add files via upload

---
+++ src/components/FilterButton.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import {View, Pressable, Text} from 'react-native';
+import styles, {colors} from '../assets/styles';
+
+const FilterButton = ({buttonLabel, searchFilters}) => {
+  const [filtered, setFiltered] = useState(true);
+
+  // searchFilters.map(item);
+
+  const buttonPressed = () => {
+    setFiltered(!filtered);
+    console.log(buttonLabel, ':', !filtered);
+  };
+
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={{
+          ...styles.buttonAct,
+          backgroundColor: filtered
+            ? colors.primary
+            : colors.backgroundColoring,
+        }}
+        onPress={buttonPressed}>
+        <Text
+          style={{
+            ...styles.buttonActText,
+            color: filtered ? colors.neutral : colors.primaryText,
+          }}>
+          {buttonLabel}
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default FilterButton;
