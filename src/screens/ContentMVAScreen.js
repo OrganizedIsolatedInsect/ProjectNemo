c5eea983fed299228c4a842332fc5a6263d933e4
LauraB <104529266+WorkAccountLB@users.noreply.github.com>
2022-09-22 10:57:37 -0700

    First Commit

--- src/screens/ContentMVAScreen.js
+++ src/screens/ContentMVAScreen.js
@@ -1,13 +1,15 @@
 /*
-
+Arrival can be from either BrowseMVAScreen or BookMarkScreen.  Items passed into ContentMVA to be returned to screen.
 */
+
 import React from 'react';
 import ContentMVA from '../components/ContentMVA';
 
-const ContentMVAScreen = ({route}) => {
-  const item = route.params.paramkey;
+const ContentMVAScreen = props => {
+  const provision = props.route.params.provision.provisionItem;
+  const lastScreen = props.route.params.provision.lastScreen;
 
-  return <ContentMVA item={item} />;
+  return <ContentMVA provisionId={provision} lastScreen={lastScreen} />;
 };
 
 export default ContentMVAScreen;
