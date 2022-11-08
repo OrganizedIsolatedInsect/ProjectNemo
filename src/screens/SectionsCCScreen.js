a0807525f3f9ad57656c3b894276ee14e6c117d2
LauraB <104529266+WorkAccountLB@users.noreply.github.com>
2022-11-08 10:56:16 -0800

    Parts List Drafted

---
+++ src/screens/SectionsCCScreen.js
@@ -0,0 +1,85 @@
+/* Parts screen - re-usable screen for the Parts List for just the Criminal Code Legislation
+ */
+
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  SectionList,
+  FlatList,
+  useWindowDimensions,
+} from 'react-native';
+import Reactotron from 'reactotron-react-native';
+
+//USER Imports
+import {CCDATAPARTS, CCDATASECTION, CCSampleTest} from '../data/dummy-data'; //for DEVELOPMENT Purposes
+import styles, {colors} from '../assets/styles';
+import CrimCodeGridList from '../components/CrimCodeGridList';
+
+import {db} from '../components/Database';
+
+import {FlashList} from '@shopify/flash-list';
+
+const SectionsCCScreen = props => {
+  const [isLoading, setIsLoading] = useState(false); //for loading spinner
+
+  const [distinctSectionList, setDistinctSectionList] = useState();
+
+  const window = useWindowDimensions();
+
+  useEffect(() => {
+    getDbData();
+  }, []);
+
+  const getDbData = () => {
+    db.transaction(tx => {
+      tx.executeSql(
+        // 'select * from (select * from CrimCode WHERE sectionHeader IS NOT NULL ORDER by section desc, sectionHeader asc) group by section',
+        'select part, section, sectionHeader from (select * from CrimCode WHERE sectionHeader IS NOT NULL ORDER by section desc, sectionHeader asc) group by section',
+        [],
+        (tx, results) => {
+          let temp = [];
+          for (let i = 0; i < results.rows.length; ++i)
+            temp.push(results.rows.item(i));
+          setDistinctSectionList(temp);
+        },
+      );
+    });
+  };
+
+  const renderList = itemdata => {
+    return (
+      <CrimCodeSectionList
+        index={itemdata.item.index}
+        part={itemdata.item.part}
+        section={itemdata.item.section}
+        sectionHeader={itemdata.item.sectionHeader}
+      />
+    );
+  };
+
+  return (
+    <View
+      style={[styles.background, styles.container, {height: window.height}]}>
+      {Reactotron.log('SectionsCC Render')}
+      <View>
+        <Text
+          style={[
+            styles.title,
+            styles.titleMargin,
+            {color: colors.primaryText},
+          ]}>
+          Criminal Code of Canada
+        </Text>
+      </View>
+      <FlashList
+        /* data={CCSampleTest} */
+        data={distinctSectionList}
+        renderItem={renderList}
+        estimatedItemSize={100}
+      />
+    </View>
+  );
+};
+
+export default SectionsCCScreen;
