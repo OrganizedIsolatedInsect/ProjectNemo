1ab84f8681f5b88c3696885f8da1c077b6a4c886
OrganizedIsolatedInsect <organizedisolatedinsect@gmail.com>
2022-11-15 12:28:29 -0800

    Content in accordion, not formatted

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import styles, {colors} from '../assets/styles';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Text, View, FlatList, VirtualizedList, ScrollView} from 'react-native';
+import {Text, View, FlatList, VirtualizedList, Pressable} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {useDispatch, useSelector} from 'react-redux';
 import {db} from './Database';
@@ -13,6 +13,7 @@ import {
   CollapseHeader,
   CollapseBody,
 } from 'accordion-collapse-react-native';
+import Accordion from 'react-native-collapsible/Accordion';
 
 import {addBookmark, removeBookmark} from '../redux/bookmarkSlice';
 import {Item} from 'react-navigation-header-buttons';
@@ -133,7 +134,64 @@ const Section = ({section, type}) => {
     return data[index];
   };
 
-  const renderAccordion = ({item, index}) => {
+  //dispatch add or remove bookmarks based bookmark icon
+  //lawtype line required to differentiate in case of duplicate Section values.
+  const dispatchAction = (section, sectionHeader) => {
+    // dispatch based on opposite of flag because marked does not change until the rerender
+    if (marked === false) {
+      dispatch(
+        addBookmark({
+          sectionLabel: sectionLabel,
+          sectionHeader: sectionHeader,
+          lawtype: 'CC',
+        }),
+      );
+    }
+    if (marked === true) {
+      dispatch(
+        removeBookmark({
+          sectionLabel: sectionLabel,
+          lawtype: 'CC',
+        }),
+      );
+    }
+  };
+
+  const [collapsedState, setCollapsedState] = useState(true);
+  // Active Infos is the section number (from react-native-collapsible, NOT our database section)
+  // This is to index the section into an array which is used can be used for the isActive state
+  // so that we can target the individual accordion icons
+  const [activeInfos, setActiveInfos] = useState([]);
+
+  const setInfos = infos => {
+    //setting up a active section state
+    setActiveInfos(infos.includes(undefined) ? [] : infos);
+    // setCollapsedState(!collapsedState);
+    setCollapsedState(prevState => !prevState);
+    // console.log('collapsedState:', collapsedState);
+  };
+
+  // prettier-ignore
+  // Props for the render must be in specific order; isActive needs to be the 3rd prop.
+  const renderHeader = (item, index, isActive, sections) => {
+    
+    return (
+      <View style={[styles.gridListItem, styles.accordionContainerHeader]}>
+                <Text>
+                {index == 0 && (
+                  <Text>{item.sectionLabel}</Text>
+                )}
+                {item.subsectionLabel}
+                {item.marginalNote}
+                </Text>     
+        {isActive ? (<Icon name="keyboard-arrow-up" size={20} /> ) : (<Icon name="keyboard-arrow-down" size={20} />)} 
+      </View>
+    );
+  };
+
+  /* eslint-disable */
+
+  const renderContent = (item, index, isActive, sections) => {
     let paraData = dbData.filter(
       (paragraph, i) => dbData[i].subsectionkey == item.subsectionKey,
     );
@@ -153,69 +211,21 @@ const Section = ({section, type}) => {
       }
     }
 
-    if (index == 0) {
-      return (
-        <View>
-          <Collapse>
-            <CollapseHeader>
-              <View style={styles.accordionContainerHeader}>
-                <Text>
-                  {item.sectionLabel}
-                  {item.subsectionLabel}
-                  {item.marginalNote}
-                </Text>
-                <Icon name="keyboard-arrow-right" size={20} />
-              </View>
-            </CollapseHeader>
-            <CollapseBody style={styles.accordionContainer}>
-              <View>
-                <Text>
-                  {item.subsectionText}
-                  {'\n'}
-                  {item.subsectionlabel} {item.subsectiontext}
-                  <FlatList
-                    data={paraFilter}
-                    keyExtractor={item => item.field1}
-                    listKey={(item2, index) => 'B' + index.toString()}
-                    renderItem={renderParagraph}
-                  />
-                </Text>
-              </View>
-            </CollapseBody>
-          </Collapse>
-        </View>
-      );
-    } else {
-      return (
-        <View>
-          <Collapse>
-            <CollapseHeader>
-              <View style={styles.accordionContainerHeader}>
-                <Text>
-                  {item.subsectionLabel}
-                  {item.marginalNote}
-                </Text>
-                <Icon name="keyboard-arrow-right" size={20} />
-              </View>
-            </CollapseHeader>
-            <CollapseBody style={styles.accordionContainer}>
-              <View>
-                <Text>
-                  {item.subsectionText}
-                  {'\n'}
-                  <FlatList
-                    data={paraFilter}
-                    keyExtractor={item => item.field1}
-                    listKey={(item2, index) => 'B' + index.toString()}
-                    renderItem={renderParagraph}
-                  />
-                </Text>
-              </View>
-            </CollapseBody>
-          </Collapse>
-        </View>
-      );
-    }
+    return (
+      <View style={styles.accordionContainer}>
+        <Text>
+          {item.subsectionText}
+          {'\n'}
+          {item.subsectionlabel} {item.subsectiontext}
+          <FlatList
+            data={paraFilter}
+            keyExtractor={item => item.field1}
+            listKey={(item2, index) => 'B' + index.toString()}
+            renderItem={renderParagraph}
+          />
+        </Text>
+      </View>
+    );
   };
 
   const renderParagraph = ({item, index}) => {
@@ -251,48 +261,30 @@ const Section = ({section, type}) => {
       </View>
     );
   };
-  //dispatch add or remove bookmarks based bookmark icon
-  //lawtype line required to differentiate in case of duplicate Section values.
-  const dispatchAction = (section, sectionHeader) => {
-    // dispatch based on opposite of flag because marked does not change until the rerender
-    if (marked === false) {
-      dispatch(
-        addBookmark({
-          sectionLabel: sectionLabel,
-          sectionHeader: sectionHeader,
-          lawtype: 'CC',
-        }),
-      );
-    }
-    if (marked === true) {
-      dispatch(
-        removeBookmark({
-          sectionLabel: sectionLabel,
-          lawtype: 'CC',
-        }),
-      );
-    }
-  };
 
   if (loading === true) {
     return (
       <View>
-        <FlatList
-          data={subsectionArray}
-          keyExtractor={(item, index) => index.toString()}
-          listKey={(item, index) => 'A' + index.toString()}
-          renderItem={renderAccordion}
+        <Accordion
+          activeSections={activeInfos}
+          //for any default active section
+          sections={subsectionArray}
+          //title and content of accordion
+          touchableComponent={Pressable}
+          //which type of touchable component you want
+          expandMultiple={true}
+          //Do you want to expand mutiple at a time or single at a time
+          renderHeader={renderHeader}
+          //Header Component(View) to render
+          renderContent={renderContent}
+          //Content Component(View) to render
+          duration={100}
+          //Duration for Collapse and expand
+          onChange={setInfos}
+          //setting the state of active sections
+          renderChildrenCollapsed={false}
+          renderAsFlatList={true}
         />
-
-        {/* <VirtualizedList
-          data={subsectionArray}
-          initialNumToRender={10}
-          renderItem={renderAccordion}
-          listKey={(item, index) => 'A' + index.toString()}
-          keyExtractor={item => item.field1}
-          getItemCount={data => data.length}
-          getItem={getItem}
-        /> */}
       </View>
     );
   }
