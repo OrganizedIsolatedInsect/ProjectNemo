a98138b94f28e1a3b903e959c2b17d81309e83ad
LauraB <104529266+WorkAccountLB@users.noreply.github.com>
2022-11-25 10:31:08 -0800

    Documentation of SectionsCCScreen.js

--- src/screens/SectionsCCScreen.js
+++ src/screens/SectionsCCScreen.js
@@ -17,13 +17,15 @@ const SectionsCCScreen = props => {
 
   let heading1KeyParam = props.route.params.passingKey;
 
-  let pagePartTitle = props.route.params.heading1Label; // Part II
-  let pagePartLabel = props.route.params.heading1TitleText; // Offences Against Public Order
+  let pagePartTitle = props.route.params.heading1Label; // example: Part II
+  let pagePartLabel = props.route.params.heading1TitleText; // example: Offences Against Public Order
 
   useEffect(() => {
     getDbData();
   }, []);
 
+  //Get distinct sections within the selected Part.  Emulates the online table of contents by using heading1key (Parts) to identify the list of sections to include
+  //Items are grouped so that the title of the sections takes priority and sql chooses the first section # from list to display.
   const getDbData = () => {
     db.transaction(tx => {
       tx.executeSql(
@@ -40,8 +42,9 @@ const SectionsCCScreen = props => {
     });
   };
 
+  //Calls the CrimCodeGridList.js component to display styles, actions.
   const renderList = itemdata => {
-    let passingName = 'SectionsCCScreen';
+    let passingName = 'SectionsCCScreen'; //identifies which screen the CrimCodeGridList.js component is being called from.
 
     return (
       <View>
