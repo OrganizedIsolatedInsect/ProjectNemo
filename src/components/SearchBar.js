4a69f337f8f044ec7d5705167f04938e748975fa
doolaik <doolaik@users.noreply.github.com>
2022-12-05 15:15:15 -0800

    SearchBar updated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,13 @@
 // Searching using Search Bar Filter in React Native List View
 // https://aboutreact.com/react-native-search-bar-filter-on-listview/
 
-import React, {useState, useEffect} from 'react';
-import {
-  View,
-  TextInput,
-  Pressable,
-  Text,
-  FlatList,
-  Button,
-  ScrollView,
-} from 'react-native';
+import React from 'react';
+import {View, TextInput, Pressable} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 import HighlightText from '@sanar/react-native-highlight-text';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
-import FilterButton from './FilterButton';
 
 import styles, {colors} from '../assets/styles';
 
@@ -30,22 +20,27 @@ import styles, {colors} from '../assets/styles';
 //      the Pressable clear button will reset the query state to blank when pressed.
 
 const SearchBar = props => {
+  const clearPressed = () => {
+    props.setQuery();
+  };
+
   return (
     <View>
       {/* Searchbar component starts here */}
       <View style={styles.searchView_Styling}>
         <TextInput
           style={styles.SearchBar_Styling}
-          // onChangeText={props.onChange}
+          onChangeText={props.setQuery}
           value={props.query}
           underlineColorAndroid="transparent"
           placeholder="Search"
+          returnKeyType="search"
         />
         {/* Search Icon with styling to position it on the left of the Searchbar */}
         <Icon name="search" size={30} style={styles.searchIcon_styling} />
         {/* When the 'Close Icon' is pressed, this will clear the contents of the Searchbar and reset the query. */}
         <Pressable
-          onPress={props.clearPressed}
+          onPress={clearPressed}
           android_ripple={styles.closeIcon_ripple_styling}
           style={styles.closeIcon_pressable_styling}>
           <Icon name="close" size={30} style={styles.closeIcon_styling} />
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -9,26 +9,14 @@ import {View} from 'react-native';
 
 import SearchBar from '../components/SearchBar';
 import FilterButton from '../components/FilterButton';
-// import InfoList from '../components/InfoList';
-
-import data from '../data/mvavt_records.json';
 
 const SearchScreen = props => {
-  const [query, setQuery] = useState('');
-  const [filteredDataSource, setFilteredDataSource] = useState(data);
-
-  const searchFilters = [
-    'Criminal Code',
-    'Motor Vehicle Act',
-    'Motor Vehicle Regulations',
-  ];
-
+  const [query, setQuery] = useState();
   return (
     <SafeAreaView style={styles.background}>
       <SearchBar
         query={query}
-        // filteredDataSource={filteredDataSource}
-        // data={data}
+        setQuery={setQuery}
       />
 
       {/* Implementation of FilterButton Component */}
@@ -37,11 +25,6 @@ const SearchScreen = props => {
         <FilterButton buttonLabel="Motor Vehicle Act" />
         <FilterButton buttonLabel="Motor Vehicle Regulations" />
       </View>
-      {/* <InfoList
-        query={query}
-        filteredDataSource={filteredDataSource}
-        // data={data}
-      /> */}
     </SafeAreaView>
   );
 };
