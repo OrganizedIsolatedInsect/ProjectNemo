a27df98bec1fda7f0ec2616a9433820061caef28
doolaik <doolaik@users.noreply.github.com>
2022-12-07 11:23:45 -0800

    Icons updated

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -15,10 +15,30 @@ export const ArrowIcon = () => {
   );
 };
 
+// Criminal Code Content Screen Icons
 export const AccordionUp = () => {
-  return <Icon name="keyboard-arrow-up" size={iconSize} color={colors.primaryText} />;
+  return (
+    <Icon name="keyboard-arrow-up" size={iconSize} color={colors.primaryText} />
+  );
 };
 
 export const AccordionDown = () => {
-  return <Icon name="keyboard-arrow-down" size={iconSize} color={colors.primaryText} />;
+  return (
+    <Icon
+      name="keyboard-arrow-down"
+      size={iconSize}
+      color={colors.primaryText}
+    />
+  );
 };
+// Criminal Code Content Screen Icons
+
+// SearchBar Icons
+export const MagnifyingGlass = () => {
+  return <Icon name="search" size={30} style={styles.searchIcon_styling} />;
+};
+
+export const CloseIcon = () => {
+  return <Icon name="close" size={30} style={styles.closeIcon_styling} />;
+};
+// SearchBar Icons
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,36 +1,28 @@
-// Searching using Search Bar Filter in React Native List View
-// https://aboutreact.com/react-native-search-bar-filter-on-listview/
-
-import React, {useState} from 'react';
+import React from 'react';
 import {View, TextInput, Pressable} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
-import HighlightText from '@sanar/react-native-highlight-text';
+import {MagnifyingGlass, CloseIcon} from '../assets/icons';
 
-import Icon from 'react-native-vector-icons/MaterialIcons';
-
-import styles, {colors} from '../assets/styles';
-
-// TODO separate the searchbar and list components
-//      figure out 'Full Text Search' (FTS4 in SQLite) which will allow us to search across all columns
-//      in a table without needing to hardcode the column names, etc.
+import styles from '../assets/styles';
 
 // SearchBar receives the query value as the state from SearchScreen and the onChange prop
 //      for the onChangeText. Additionally, receives the clearButtonPressed function so that
 //      the Pressable clear button will reset the query state to blank when pressed.
 
 const SearchBar = props => {
+  // Function to clear all text (and state) entered into the searchbar
   const clearPressed = () => {
     props.setQuery();
+    props.setSearchTerm();
   };
-
+  // Pass the actual searched term back to the search screen and results
   const submitSearchTerm = () => {
     props.setSearchTerm(props.query);
   };
 
   return (
     <View>
-      {/* Searchbar component starts here */}
       <View style={styles.searchView_Styling}>
         <TextInput
           style={styles.SearchBar_Styling}
@@ -42,16 +34,15 @@ const SearchBar = props => {
           onSubmitEditing={submitSearchTerm}
         />
         {/* Search Icon with styling to position it on the left of the Searchbar */}
-        <Icon name="search" size={30} style={styles.searchIcon_styling} />
+        <MagnifyingGlass />
         {/* When the 'Close Icon' is pressed, this will clear the contents of the Searchbar and reset the query. */}
         <Pressable
           onPress={clearPressed}
           android_ripple={styles.closeIcon_ripple_styling}
           style={styles.closeIcon_pressable_styling}>
-          <Icon name="close" size={30} style={styles.closeIcon_styling} />
+          <CloseIcon />
         </Pressable>
       </View>
-      {/* Searchbar component ends here */}
     </View>
   );
 };
