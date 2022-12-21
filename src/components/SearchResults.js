b87a471080bc46d9225a20847f7e31ac6757eea2
PizzaCat07 <58643574+PizzaCat07@users.noreply.github.com>
2022-12-20 17:40:29 -0800

    added setTimeOut as test

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -56,6 +56,9 @@ const SearchResults = ({searchQueryTerm, filterArray}) => {
   }, [mvaDbData, crimCodeDbData]);
 
   useEffect(() => {
+    /*   setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); */
     setIsLoading(false);
   }, [mvaRegulationRenderData, subsectionData, mvaRenderData]);
 
