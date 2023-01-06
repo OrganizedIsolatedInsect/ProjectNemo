26817c2493732848e77a7e849fe5a1281696a7d4
doolaik <doolaik@users.noreply.github.com>
2023-01-05 16:44:58 -0800

    Initial

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,14 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useTransition, Suspense} from 'react';
 import styles from '../assets/styles';
-import {
-  View,
-  Text,
-  FlatList,
-  VirtualizedList,
-  SectionList,
-  Pressable,
-  ActivityIndicator,
-} from 'react-native';
+import {View, Text, FlatList, Pressable, ActivityIndicator} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 import {
@@ -31,13 +23,15 @@ const SearchResults = ({searchQueryTerm, filterArray}) => {
   const [mvaRenderData, setMvaRenderData] = useState([]);
   const [isloading, setIsLoading] = useState(true);
 
+  const [isPending, startTransition] = useTransition();
+
   const navAid = useNavigation();
 
   useEffect(() => {
     setIsLoading(true);
     setSearchResults(searchQueryTerm);
     getDbData(searchResults);
-  }, [searchQueryTerm]);
+  }, [searchQueryTerm, searchResults]);
 
   useEffect(() => {
     const transformData = async () => {
@@ -59,6 +53,8 @@ const SearchResults = ({searchQueryTerm, filterArray}) => {
     transformData();
   }, [mvaDbData, crimCodeDbData]);
 
+  console.log('isPending start: ', isPending);
+
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
@@ -71,52 +67,55 @@ const SearchResults = ({searchQueryTerm, filterArray}) => {
     const sqlSearch = `%${searchResults}%`;
     //each ? requires its only index in the array that passes to the executeSql command
     //SQL Query for crim code data
-    db.transaction(tx => {
-      tx.executeSql(
-        'SELECT * from CCDataV2 WHERE sectionText like ? or subsectionText like ? or marginalNote like ? or paragraphText like ? or subparagraphText like ? or clauseText like ? or subclauseText like ?',
-        [
-          sqlSearch,
-          sqlSearch,
-          sqlSearch,
-          sqlSearch,
-          sqlSearch,
-          sqlSearch,
-          sqlSearch,
-        ],
-        (tx, results) => {
-          const crimCodeTemp = [];
-          const searchCountTemp = [];
+
+    startTransition(() => {
+      db.transaction(tx => {
+        tx.executeSql(
+          'SELECT * from CCDataV2 WHERE sectionText like ? or subsectionText like ? or marginalNote like ? or paragraphText like ? or subparagraphText like ? or clauseText like ? or subclauseText like ?',
+          [
+            sqlSearch,
+            sqlSearch,
+            sqlSearch,
+            sqlSearch,
+            sqlSearch,
+            sqlSearch,
+            sqlSearch,
+          ],
+          (tx, results) => {
+            const crimCodeTemp = [];
+            const searchCountTemp = [];
+            for (let i = 0; i < results.rows.length; ++i) {
+              crimCodeTemp.push(results.rows.item(i));
+              searchCountTemp.push(results.rows.item(i).sectionLabel);
+            }
+            setCrimCodeSearchCount(searchCountTemp);
+            setCrimCodeDbData(crimCodeTemp);
+          },
+        );
+      });
+      //SQL query for MVA fine data
+      db.transaction(tx => {
+        tx.executeSql(
+          'SELECT * from MVA where contravention like ? or sectionText like ? or sectionSubsection like ? or sectionParagraph like ? or sectionSubparagraph like ? ',
+          [sqlSearch, sqlSearch, sqlSearch, sqlSearch, sqlSearch],
+          (tx, results) => {
+            const mvaTemp = [];
+            for (let i = 0; i < results.rows.length; ++i) {
+              mvaTemp.push(results.rows.item(i));
+            }
+            setMvaDbData(mvaTemp);
+          },
+        );
+      });
+      //SQL for legislation index
+      db.transaction(tx => {
+        tx.executeSql('SELECT * from LegislationIndex', [], (tx, results) => {
+          const dbIndexTemp = [];
           for (let i = 0; i < results.rows.length; ++i) {
-            crimCodeTemp.push(results.rows.item(i));
-            searchCountTemp.push(results.rows.item(i).sectionLabel);
+            dbIndexTemp.push(results.rows.item(i));
           }
-          setCrimCodeSearchCount(searchCountTemp);
-          setCrimCodeDbData(crimCodeTemp);
-        },
-      );
-    });
-    //SQL query for MVA fine data
-    db.transaction(tx => {
-      tx.executeSql(
-        'SELECT * from MVA where contravention like ? or sectionText like ? or sectionSubsection like ? or sectionParagraph like ? or sectionSubparagraph like ? ',
-        [sqlSearch, sqlSearch, sqlSearch, sqlSearch, sqlSearch],
-        (tx, results) => {
-          const mvaTemp = [];
-          for (let i = 0; i < results.rows.length; ++i) {
-            mvaTemp.push(results.rows.item(i));
-          }
-          setMvaDbData(mvaTemp);
-        },
-      );
-    });
-    //SQL for legislation index
-    db.transaction(tx => {
-      tx.executeSql('SELECT * from LegislationIndex', [], (tx, results) => {
-        const dbIndexTemp = [];
-        for (let i = 0; i < results.rows.length; ++i) {
-          dbIndexTemp.push(results.rows.item(i));
-        }
-        setDbIndex(dbIndexTemp);
+          setDbIndex(dbIndexTemp);
+        });
       });
     });
   };
@@ -134,128 +133,150 @@ const SearchResults = ({searchQueryTerm, filterArray}) => {
   },
   {});
 
-  if (isloading === true) {
+  const LoadingSearchResults = () => {
     return (
       <View style={styles.spinnerContainer}>
         <ActivityIndicator size={'large'} />
       </View>
     );
-  }
+  };
 
-  return (
-    <View>
-      <FlatList
-        data={dbIndex}
-        keyExtractor={data => data.legislationId}
-        renderItem={({item}) => {
-          if (
-            item.legislationType === 'CrimCode' &&
-            filterArray.some(
-              legislation =>
-                legislation.type === 'Criminal Code' &&
-                legislation.filterState === false,
-            )
-          ) {
-            return (
-              <View>
-                <Text style={styles.heading_2}>{item.legislationTitle}</Text>
+  const SearchResultsLoaded = () => {
+    //   return (
+    //     <View>
+    //       <Text>asdlflasdkfj;alskdf;lasdjf;laksdjf;lakdjf;lasf</Text>
+    //     </View>
+    //   );
+    // };
+
+    return (
+      <View>
+        <FlatList
+          data={dbIndex}
+          keyExtractor={data => data.legislationId}
+          renderItem={({item}) => {
+            if (
+              item.legislationType === 'CrimCode' &&
+              filterArray.some(
+                legislation =>
+                  legislation.type === 'Criminal Code' &&
+                  legislation.filterState === false,
+              )
+            ) {
+              return (
                 <View>
+                  <Text style={styles.heading_2}>{item.legislationTitle}</Text>
+                  <View>
+                    <FlatList
+                      style={styles.searchResultsContainer}
+                      data={subsectionData}
+                      keyExtractor={data => data.field1}
+                      renderItem={({item, index}) => {
+                        return (
+                          <View key={index}>
+                            <Pressable
+                              onPress={() => {
+                                navAid.navigate('ContentCCScreen', {
+                                  heading2Key: item.heading2Key,
+                                  searchResults: searchResults,
+                                  marginalNoteKey: item.marginalNoteKey,
+                                });
+                              }}>
+                              <View style={styles.heading_2}>
+                                <CrimCodeRenderHeader
+                                  subsectionData={item}
+                                  searchResults={searchResults}
+                                />
+                              </View>
+                              <View>
+                                <CrimCodeRenderBody
+                                  subsectionData={item}
+                                  dbData={crimCodeDbData}
+                                  searchResults={searchResults}
+                                />
+                              </View>
+                            </Pressable>
+                          </View>
+                        );
+                      }}
+                    />
+                  </View>
+                </View>
+              );
+            }
+            if (
+              item.legislationType === 'MVA' &&
+              filterArray.some(
+                legislation =>
+                  legislation.type === 'Motor Vehicle Regulations' &&
+                  legislation.filterState === false,
+              )
+            ) {
+              return (
+                <View>
+                  <Text style={styles.heading_2}>{item.legislationTitle}</Text>
                   <FlatList
-                    style={styles.searchResultsContainer}
-                    data={subsectionData}
-                    keyExtractor={data => data.field1}
+                    data={mvaRegulationRenderData}
+                    keyExtractor={data => data.index}
                     renderItem={({item, index}) => {
                       return (
-                        <View key={index}>
-                          <Pressable
-                            onPress={() => {
-                              navAid.navigate('ContentCCScreen', {
-                                heading2Key: item.heading2Key,
-                                searchResults: searchResults,
-                                marginalNoteKey: item.marginalNoteKey,
-                              });
-                            }}>
-                            <View style={styles.heading_2}>
-                              <CrimCodeRenderHeader
-                                subsectionData={item}
-                                searchResults={searchResults}
-                              />
-                            </View>
-                            <View>
-                              <CrimCodeRenderBody
-                                subsectionData={item}
-                                dbData={crimCodeDbData}
-                                searchResults={searchResults}
-                              />
-                            </View>
-                          </Pressable>
+                        <View key={index} style={styles.searchResultsContainer}>
+                          <ContentMVA
+                            provisionId={item.provision}
+                            searchResults={searchResults}
+                          />
                         </View>
                       );
                     }}
                   />
                 </View>
-              </View>
-            );
-          }
-          if (
-            item.legislationType === 'MVA' &&
-            filterArray.some(
-              legislation =>
-                legislation.type === 'Motor Vehicle Regulations' &&
-                legislation.filterState === false,
-            )
-          ) {
-            return (
-              <View>
-                <Text style={styles.heading_2}>{item.legislationTitle}</Text>
-                <FlatList
-                  data={mvaRegulationRenderData}
-                  keyExtractor={data => data.index}
-                  renderItem={({item, index}) => {
-                    return (
-                      <View key={index} style={styles.searchResultsContainer}>
-                        <ContentMVA
-                          provisionId={item.provision}
-                          searchResults={searchResults}
-                        />
-                      </View>
-                    );
-                  }}
-                />
-              </View>
-            );
-          }
+              );
+            }
 
-          if (
-            item.legislationType === 'MVA' &&
-            filterArray.some(
-              legislation =>
-                legislation.type === 'Motor Vehicle Act' &&
-                legislation.filterState === false,
-            )
-          ) {
-            return (
-              <View>
-                <Text style={styles.heading_2}>{item.legislationTitle}</Text>
-                <FlatList
-                  data={mvaRenderData}
-                  keyExtractor={data => data.index}
-                  renderItem={({item, index}) => {
-                    return (
-                      <View key={index} style={styles.searchResultsContainer}>
-                        <ContentMVA
-                          provisionId={item.provision}
-                          searchResults={searchResults}
-                        />
-                      </View>
-                    );
-                  }}
-                />
-              </View>
-            );
-          }
-        }}
-      />
+            if (
+              item.legislationType === 'MVA' &&
+              filterArray.some(
+                legislation =>
+                  legislation.type === 'Motor Vehicle Act' &&
+                  legislation.filterState === false,
+              )
+            ) {
+              return (
+                <View>
+                  <Text style={styles.heading_2}>{item.legislationTitle}</Text>
+                  <FlatList
+                    data={mvaRenderData}
+                    keyExtractor={data => data.index}
+                    renderItem={({item, index}) => {
+                      return (
+                        <View key={index} style={styles.searchResultsContainer}>
+                          <ContentMVA
+                            provisionId={item.provision}
+                            searchResults={searchResults}
+                          />
+                        </View>
+                      );
+                    }}
+                  />
+                </View>
+              );
+            }
+          }}
+        />
+      </View>
+    );
+  };
+
+  console.log(isPending);
+
+  return (
+    <View>
+      {/* {!isPending ? <LoadingSearchResults /> : <SearchResultsLoaded />} */}
+      {/* {isPending ? <SearchResultsLoaded /> : <LoadingSearchResults />} */}
+      {/* <LoadingSearchResults /> */}
+      <Suspense fallback={<LoadingSearchResults />}>
+        <SearchResultsLoaded />
+      </Suspense>
     </View>
   );
 };
